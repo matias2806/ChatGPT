2b7761472a022e2c8756849d64f585b0eb3ff4ca
matiasPalmieri <86312764+matiasPalmieri@users.noreply.github.com>
2023-09-06 13:02:35 -0300

    create gitignore

---
+++ ChatGPT/ChatGPT/.gitignore.swift
@@ -0,0 +1,8 @@
+//
+//  .gitignore.swift
+//  ChatGPT
+//
+//  Created by Matias Palmieri on 06/09/2023.
+//
+
+import Foundation
---
+++ ChatGPT/ChatGPT/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  ChatGPT
+//
+//  Created by Matias Palmieri on 06/09/2023.
+//
+
+import Foundation
+
+struct OpenAIKeys {
+    static let apiKey = "tu_api_key_aqui" // Reemplaza con tu API Key de OpenAI
+}
